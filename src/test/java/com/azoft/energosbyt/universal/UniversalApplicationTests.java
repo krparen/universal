e4b0fd122555af633866fc7469812e8dab510685
g.eremeev <g.eremeev@azoft.com>
2020-09-10 07:58:38 +0300

    Initial commit

---
+++ src/test/java/com/azoft/energosbyt/universal/UniversalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azoft.energosbyt.universal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
