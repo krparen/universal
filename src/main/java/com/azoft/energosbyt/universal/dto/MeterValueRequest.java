7dafd50adb01c09577bd6d830a58cbdddbde0da9
g.eremeev <g.eremeev@azoft.com>
2020-09-11 07:34:24 +0300

    small date deserialization fixes

--- src/main/java/com/azoft/energosbyt/universal/dto/MeterValueRequest.java
+++ src/main/java/com/azoft/energosbyt/universal/dto/MeterValueRequest.java
@@ -1,5 +1,6 @@
 package com.azoft.energosbyt.universal.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,12 +10,12 @@ import org.springframework.format.annotation.DateTimeFormat;
 @Data
 public class MeterValueRequest {
 
-  private static final String DATE_TIME_FORMAT = "dd.mm.yyyy HH:mm:ss";
+  private static final String DATE_TIME_FORMAT = "dd.MM.yyyy HH:mm:ss";
 
   private String system;
   private String account;
   private String txnId;
-  @DateTimeFormat(pattern = DATE_TIME_FORMAT) // отвечает за десериализацию
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = DATE_TIME_FORMAT, timezone = "Europe/Moscow")
   private LocalDateTime dateMv;
 
   private List<Meter> meterValues = new ArrayList<>();
--- src/main/java/com/azoft/energosbyt/universal/dto/PayRequest.java
+++ src/main/java/com/azoft/energosbyt/universal/dto/PayRequest.java
@@ -1,24 +1,24 @@
 package com.azoft.energosbyt.universal.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.springframework.format.annotation.DateTimeFormat;
 
 @Getter
 @Setter
 @ToString
 public class PayRequest {
 
-  private static final String DATE_TIME_FORMAT = "dd.mm.yyyy HH:mm:ss";
+  private static final String DATE_TIME_FORMAT = "dd.MM.yyyy HH:mm:ss";
 
   private String system;
   private String account;
   private String txnId;
 
-  @DateTimeFormat(pattern = DATE_TIME_FORMAT) // отвечает за десериализацию
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = DATE_TIME_FORMAT, timezone = "Europe/Moscow")
   private LocalDateTime datePay;
 
   @Data
