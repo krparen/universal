ce7e0cf51a381c7790c4cffc97fca687eacd3729
krparen <ksackk@gmail.com>
2020-11-09 11:48:12 +0300

    universal rest exception response format

--- src/main/java/com/azoft/energosbyt/universal/exception/ApplicationExceptionHandler.java
+++ src/main/java/com/azoft/energosbyt/universal/exception/ApplicationExceptionHandler.java
@@ -1,24 +1,69 @@
 package com.azoft.energosbyt.universal.exception;
+
 import com.azoft.energosbyt.universal.dto.BasicResponse;
 import com.azoft.energosbyt.universal.dto.OperationStatus;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.*;
 
 @ControllerAdvice
 public class ApplicationExceptionHandler {
 
-  @ExceptionHandler(ApiException.class)
-  public ResponseEntity<BasicResponse> handleApiException(ApiException exception) {
-    BasicResponse response = new BasicResponse();
-    response.setStatus(OperationStatus.error);
-    response.setErrorCode(exception.getErrorCode().getStringValue());
-    if (exception.isUseMessageAsComment()) {
-      response.setErrorMessage(exception.getMessage());
+    @ResponseStatus(OK)
+    @ResponseBody
+    @ExceptionHandler(ApiException.class)
+    public BasicResponse handleApiException(ApiException exception) {
+        BasicResponse response = new BasicResponse();
+        response.setStatus(OperationStatus.error);
+        response.setErrorCode(exception.getErrorCode().getStringValue());
+        if (exception.isUseMessageAsComment()) {
+            response.setErrorMessage(exception.getMessage());
+        }
+
+        return response;
     }
 
-    return ResponseEntity.status(HttpStatus.OK)
-        .body(response);
-  }
+    @ResponseStatus(BAD_REQUEST)
+    @ResponseBody
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public BasicResponse methodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        BindingResult result = ex.getBindingResult();
+        List<FieldError> fieldErrors = result.getFieldErrors();
+
+        String errorMessage = fieldErrors.stream()
+                .map(this::constructFieldErrorMessage)
+                .reduce((s1, s2) -> s1 + "; " + s2)
+                .orElse(null);
+
+        BasicResponse response = new BasicResponse();
+        response.setStatus(OperationStatus.error);
+        response.setErrorCode(ErrorCode.UNEXPECTED_ERROR.getStringValue());
+        response.setErrorMessage(errorMessage);
+
+        return response;
+    }
+
+    private String constructFieldErrorMessage(FieldError fieldError) {
+        return "field " + "'" + fieldError.getField() + "'" + " " + fieldError.getDefaultMessage();
+    }
+
+
+    @ResponseStatus(INTERNAL_SERVER_ERROR)
+    @ResponseBody
+    @ExceptionHandler(Exception.class)
+    public BasicResponse handleUnexpectedException(Exception exception) {
+        BasicResponse response = new BasicResponse();
+        response.setStatus(OperationStatus.error);
+        response.setErrorCode(ErrorCode.UNEXPECTED_ERROR.getStringValue());
+        response.setErrorMessage(exception.getMessage());
+        return response;
+    }
 }
